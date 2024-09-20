5ed7c5112fc08baee8ca83abece005d11bb66758
Fabio Issamu Oshiro <fabio.oshiro@gmail.com>
2024-09-20 20:06:15 -0300

    fix: removing hardcoded header param

--- src/cartesify/AxiosLikeClientV2.ts
+++ src/cartesify/AxiosLikeClientV2.ts
@@ -36,7 +36,6 @@ export class AxiosLikeClientV2 {
             urlInner.pathname += `/${jsonEncoded}`;
             const response = await axios.get(urlInner.href, {
                 headers: {
-                    "x-my-header": "some-value",
                     "Content-Type": "application/json",
                     Accept: "application/json",
                 },
