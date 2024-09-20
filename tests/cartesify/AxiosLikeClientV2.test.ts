364d8e5897327b29a2ae1fdc59ff34b713e56402
Fabio Issamu Oshiro <fabio.oshiro@gmail.com>
2024-09-20 20:55:52 -0300

    ci: real axios behavior

--- tests/cartesify/AxiosLikeClientV2.test.ts
+++ tests/cartesify/AxiosLikeClientV2.test.ts
@@ -1,30 +1,38 @@
 import { expect, it, describe, beforeAll } from "@jest/globals";
 import { Cartesify } from "../../src";
 import { ethers } from "ethers";
+import { AxiosClient } from "../../src/models/config";
+import axios from "axios";
 
 describe("AxiosLikeClientV2", () => {
     const TEST_TIMEOUT = 300000
-    let axiosLikeClient: any
+
+    // to test the behavior with real axios
+    let axiosLikeClient = axios
+
+    // let axiosLikeClient: AxiosClient
 
     beforeAll(() => {
         const provider = ethers.getDefaultProvider("http://localhost:8545");
         const privateKey = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'
         let signer = new ethers.Wallet(privateKey, provider);
-        axiosLikeClient = Cartesify.createAxios({
-            dappAddress: '0xab7528bb862fb57e8a2bcd567a2e929a0be56a5e',
-            endpoints: {
-                graphQL: new URL("http://localhost:8080/graphql"),
-                inspect: new URL("http://localhost:8080/inspect"),
-            },
-            provider,
-            signer
-        })
+
+        // to test the behavior of real axios the code below is commented
+        // axiosLikeClient = Cartesify.createAxios({
+        //     dappAddress: '0xab7528bb862fb57e8a2bcd567a2e929a0be56a5e',
+        //     endpoints: {
+        //         graphQL: new URL("http://localhost:8080/graphql"),
+        //         inspect: new URL("http://localhost:8080/inspect"),
+        //     },
+        //     provider,
+        //     signer
+        // })
     })
 
     it("should work with GET", async () => {
         const response = await axiosLikeClient.get("http://127.0.0.1:8383/health")
         expect(response.statusText.toLowerCase()).toBe('ok')
-        const json = await response.data;
+        const json = response.data;
         expect(json.some).toEqual('response')
     }, TEST_TIMEOUT)
 
@@ -35,7 +43,7 @@ describe("AxiosLikeClientV2", () => {
             },
         })
         expect(response.statusText.toLowerCase()).toBe('ok')
-        const json = await response.data;
+        const json = response.data;
         expect(json).toEqual({ myPost: { any: "body" } })
     }, TEST_TIMEOUT)
 
@@ -46,7 +54,7 @@ describe("AxiosLikeClientV2", () => {
             },
         })
         expect(response.statusText.toLowerCase()).toBe('ok')
-        const json = await response.data;
+        const json = response.data;
         expect(json).toEqual({ updateBody: { any: "body" } })
     }, TEST_TIMEOUT)
 
@@ -57,27 +65,28 @@ describe("AxiosLikeClientV2", () => {
             }
         })
         expect(response.statusText.toLowerCase()).toBe('ok')
-        const json = await response.data;
+        const json = response.data;
         expect(json).toEqual({ patchBody: { any: "body" } })
-        expect(response.headers.get('content-type')).toContain('application/json')
+        expect(response.headers['content-type']).toContain('application/json')
     }, TEST_TIMEOUT)
 
     it("should work with DELETE", async () => {
         const response = await axiosLikeClient.delete("http://127.0.0.1:8383/delete?foo=bar")
         expect(response.statusText.toLowerCase()).toBe('ok')
-        const json = await response.data;
+        const json = response.data;
         expect(json).toEqual({ query: { foo: "bar" } })
     }, TEST_TIMEOUT)
 
     it("should handle 404 doing POST", async () => {
-        const response = await axiosLikeClient.post("http://127.0.0.1:8383/echoNotFound", { any: 'body' }, {
+        const error = await axiosLikeClient.post("http://127.0.0.1:8383/echoNotFound", { any: 'body' }, {
             headers: {
                 "Content-Type": "application/json",
             }
-        })
+        }).catch(e => e)
 
-        expect(response.statusText.toLowerCase()).toBe('not found')
-        expect(response.status).toBe(404)
+        expect(error).toBeInstanceOf(Error)
+        expect(error.message).toBe('Request failed with status code 404')
+        expect(error.response.status).toBe(404)
     }, TEST_TIMEOUT)
 
     it("should handle 'TypeError: fetch failed' doing POST. Connection refused", async () => {
@@ -87,8 +96,8 @@ describe("AxiosLikeClientV2", () => {
             }
         }).catch((e: Error) => e)
 
-        expect(error.constructor.name).toBe("TypeError")
-        expect(error.message).toBe("fetch failed")
+        expect(error.constructor.name).toBe("AxiosError")
+        expect((error as any).message).toBe("connect ECONNREFUSED 127.0.0.1:12345")
     }, TEST_TIMEOUT)
 
     it("should handle 'Error: connect ECONNREFUSED 127.0.0.1:12345' doing GET. Connection refused", async () => {
@@ -98,7 +107,7 @@ describe("AxiosLikeClientV2", () => {
         expect(error.message).toBe("connect ECONNREFUSED 127.0.0.1:12345")
     }, TEST_TIMEOUT)
 
-    it("should send the msg_sender as x-msg_sender within the headers. Also send other metadata with 'x-' prefix", async () => {
+    it.skip("should send the msg_sender as x-msg_sender within the headers. Also send other metadata with 'x-' prefix", async () => {
         const response = await axiosLikeClient.post("http://127.0.0.1:8383/echo/headers", { any: 'body' }, {
             headers: {
                 "Content-Type": "application/json",
@@ -115,7 +124,7 @@ describe("AxiosLikeClientV2", () => {
 
     it("should send the headers doing GET", async () => {
         const response = await axiosLikeClient.get("http://127.0.0.1:8383/echo/headers", {
-            headers: { "x-my-header": "some-value"}
+            headers: { "x-my-header": "some-value" }
         })
         expect(response.statusText.toLowerCase()).toBe('ok')
         expect(response.config.headers['x-my-header']).toEqual('some-value')
