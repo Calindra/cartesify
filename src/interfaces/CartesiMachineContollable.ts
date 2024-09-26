c63520420d024a493041e3695191144361d6b88c
Vinicius Catarino <vinicius.catarino@calindra.com.br>
2024-09-26 18:51:27 -0300

    chore: change method instructions

--- src/interfaces/CartesiMachineContollable.ts
+++ src/interfaces/CartesiMachineContollable.ts
@@ -1,13 +1,15 @@
 export interface CartesiMachineControllable {
     /**
-     * Inspects the current state of the Cartesi machine.
-     * @returns A promise that resolves with the inspection result.
+     * Performs both the inspection and advancement of the Cartesi machine.
+     * This method inspects the current state of the machine and then advances 
+     * its state in a single operation.
+     * 
+     * - **Inspect**: Retrieves the machine's current state for analysis or debugging.
+     * - **Advance**: Progresses the machine's state, potentially altering its behavior or state for future operations.
+     * 
+     * @returns A promise that resolves with the result of the inspection 
+     * after the machine has been advanced. The result may include details 
+     * from both the inspection and advancement processes.
      */
     operateMachine(): Promise<any>;
-
-    /**
-     * Advances the state of the Cartesi machine.
-     * @returns A promise that resolves when the machine has advanced.
-     */
-    // advanceCartesiMachine(): Promise<any>;
 }
