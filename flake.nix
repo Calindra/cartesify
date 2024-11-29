23b2ae9429222d2e7b17876722cd2a5f6254bb16
sandhilt <6170125+sandhilt@users.noreply.github.com>
2024-11-29 14:55:25 -0300

    feat: add flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Node.js development environment with PNPM";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_22
+            pnpm
+          ];
+
+          shellHook = ''
+            echo "Node.js and PNPM development environment"
+            node --version
+            pnpm --version
+          '';
+        };
+      }
+    );
+}
